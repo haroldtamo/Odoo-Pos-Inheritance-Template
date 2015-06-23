a3c6feaf999fdbd671546ec8f60b034f9f2a01f8
Harold TAMO <h.tamo@y-note.cm>
2015-06-23 11:51:53 +0100

    [IMP] import classes

---
+++ pos_template/static/src/js/main.js
@@ -0,0 +1,17 @@
+openerp.pos_template = function(instance) {
+
+    var module = instance.pos_template; // Create namespace of pos_template for your own class
+
+    var pos_base = instance.point_of_sale; // Namespace of point_of_sale module that holds classes of the POS
+
+    pos_template_db(instance,module);         // import db.js
+
+    pos_template_models(instance,module);     // import models.js
+
+    pos_template_screens(instance,module);    // import screens.js
+
+    pos_template_widgets(instance,module);    // import widgets.js
+
+};
+
+
